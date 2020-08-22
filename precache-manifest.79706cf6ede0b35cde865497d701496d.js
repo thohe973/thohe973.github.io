2c9dad27fd04cc1428f7b8a1b553b0ecc9ad9d1d
Thomas Henriksson <thohe973@student.liu.se>
2020-08-22 10:43:36 +0200

    initial commit

---
+++ precache-manifest.79706cf6ede0b35cde865497d701496d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc00249bec5cbc7488fced22a726caa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68969487428dfccd28b",
+    "url": "/static/css/main.f00d75d2.chunk.css"
+  },
+  {
+    "revision": "cb1baf3e31a4c12a5ac8",
+    "url": "/static/js/2.04d42004.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/static/js/2.04d42004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68969487428dfccd28b",
+    "url": "/static/js/main.f0284bd9.chunk.js"
+  },
+  {
+    "revision": "71d0858ae9d019336cdf",
+    "url": "/static/js/runtime-main.b9889f0d.js"
+  }
+]);
